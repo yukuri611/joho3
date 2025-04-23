c24a1f023702911e44038dfdecc20c741de8fb54
yukuri611 <yukiiw611@gmail.com>
2025-04-24 08:52:14 +0900

    latest update(4/21)

--- ex3_rv32/rv_test/program2.c
+++ ex3_rv32/rv_test/program2.c
@@ -5,40 +5,47 @@
 
 #define BUF_SIZE  9
 
-void store_int(int*);
+
+void store_int(int*, int*);
 
 
 int main() {
 #if defined(NATIVE_MODE)
     printf("do nothing...\n");
 #else
-    while(1) {
+    start_profiler();
+    int terminated = 0;
+    while(!terminated) {
         int a;
         printf("Number A: ");
-        store_int(&a);
+        store_int(&a, &terminated);
+        if (terminated) break;
         printf("a: %d\n", a);
 
         int b;
         printf("Number B: ");
-        store_int(&b);
+        store_int(&b, &terminated);
+        if (terminated) break;
         printf("b: %d\n", b);
 
         if (b != 0) {
-            printf("result / : %f\n", a / b);
+            printf("result / : %f\n", (float)a/b);
             printf("mod result : %d\n", a % b);
         }
         else {
             printf("Error. Cannot divide with 0\n");
         }
     }
+    printf("End program\n");
 
+    end_profiler();
     
 
 #endif
     return 0;
 }
 
-void store_int(int* store) {
+void store_int(int* store, int* terminated) {
 #if defined(NATIVE_MODE)
 #else
     char rx_buf[BUF_SIZE];
@@ -47,6 +54,13 @@ void store_int(int* store) {
         char ch = io_getch(); //teratermに入力された数値(char型)1つを取得
         
         rx_buf[rx_pos++] = ch; //bufferに保存
+
+        if (ch == TERMINATE_CODE) {
+            rx_buf[rx_pos] = 0;
+            printf("%s\nCtrl-Z detected...\n", rx_buf);
+            *terminated = 1;
+            break;
+        }
 
         //改行入力か、bufferの最後（終端文字分を除く）まで到達したとき、while文を抜ける
         if (ch == '\n' || rx_pos == BUF_SIZE - 1) {
--- ex3_rv32/rv_test/program5.c
+++ ex3_rv32/rv_test/program5.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <string.h>
 #include "io.h"
 
 #if !defined(NATIVE_MODE)
@@ -16,13 +17,13 @@ char convert_char_seg7(int ch);
 void set_gpio_string(U64 val);
 unsigned counter = 0;
 
-const char *string_list[] = {"Apple", "Banana", "Cherry", "Date"};
+const char *string_list[] = {"apple", "banana", "cherry", "date"};
 int size = sizeof(string_list) / sizeof(string_list[0]);
 
 void timer_handler() {
     counter ++;
-    printf("counting\n");
     const char* s = string_list[counter % size];
+    //set_gpio_stringに長さ8文字の文字列を与えるために、paddingを使用する
     char padded[9] = "        "; // 空白8文字 + null終端
     strncpy(padded, s, 8);
     set_gpio_string(GPIO_STR8_new(padded));
@@ -33,6 +34,8 @@ int main() {
 #if defined(NATIVE_MODE)
     printf("do nothing...\n");
 #else
+    start_profiler();
+    int terminated = 0;
     set_mtimer_interval(1000/*msec*/); 
     // time_interrupt_hookは、timer_intervalごとに実行される割り込み関数のポインタ。
     // timer_interrupt_hookに、上で作成したtimer_handler関数を割り当てる。
@@ -40,23 +43,30 @@ int main() {
     enable_timer_interrupt(); //割り込みハンドラを設定するための関数
     int timer_switch = 1; //timer_switchが1のとき、timerによる割り込みが可能
     printf("timer on\n");
-    while (1) {
-        if (io_getch()) { //Teratermからの入力を待つ
-            timer_switch = !timer_switch;   // timer_switchが切り替わる
-            if (timer_switch) {
-                asm volatile("csrs mie, %[new]" : : [new] "r" (0x00000080));	///	set meie[7] : machine-external-interrupt-enable
-                printf("timer on\n");
-            }
-            else {
-                asm volatile("csrc mie, %[new]" : : [new] "r" (0x00000080));	///	clear meie[7] : machine-external-interrupt-enable
-                printf("timer off\n");
-                const char* s = "timeroff";
-                char padded[9] = "        "; 
-                strncpy(padded, s, 8);
-                set_gpio_string(GPIO_STR8_new(padded)); //7-SEGにtimeroffと表示
-            }
+    while (!terminated) {
+        char ch = io_getch();
+        if (ch == TERMINATE_CODE) {
+            printf("Ctrl-Z detected...\n");
+            asm volatile("csrc mie, %[new]" : : [new] "r" (0x00000080));	///	clear meie[7] : machine-external-interrupt-enable
+            printf("timer off\n");
+            terminated = 1;
+            break;
+        }
+        timer_switch = !timer_switch;   // timer_switchが切り替わる
+        if (timer_switch) {
+            asm volatile("csrs mie, %[new]" : : [new] "r" (0x00000080));	///	set meie[7] : machine-external-interrupt-enable
+            printf("timer on\n");
+        }
+        else {
+            asm volatile("csrc mie, %[new]" : : [new] "r" (0x00000080));	///	clear meie[7] : machine-external-interrupt-enable
+            printf("timer off\n");
+            const char* s = "timeroff";
+            char padded[9] = "        "; 
+            strncpy(padded, s, 8);
+            set_gpio_string(GPIO_STR8_new(padded)); //7-SEGにtimeroffと表示
         }
     }
+    end_profiler();
 #endif
     return 0;
 }
