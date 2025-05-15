75cacee1527bc67d9cb217449bf0cadeaaa80324
yukuri611 <yukiiw611@gmail.com>
2025-05-15 09:21:45 +0900

    withoutUARTを作成。同じteratermから連続入力を受け取った時の処理ができてない。

--- ex3_rv32/rv_test/C2R_ROOT/RTL_V_TB/RV32I_ARCH1_step_MemInitFileName.v
+++ ex3_rv32/rv_test/C2R_ROOT/RTL_V_TB/RV32I_ARCH1_step_MemInitFileName.v
@@ -1,4 +1,4 @@
-`define MEM_INIT_G__this_mem_m_0 "task2/G__this_mem_m_0.log"
-`define MEM_INIT_G__this_mem_m_1 "task2/G__this_mem_m_1.log"
-`define MEM_INIT_G__this_mem_m_2 "task2/G__this_mem_m_2.log"
-`define MEM_INIT_G__this_mem_m_3 "task2/G__this_mem_m_3.log"
+`define MEM_INIT_G__this_mem_m_0 "withoutUART/G__this_mem_m_0.log"
+`define MEM_INIT_G__this_mem_m_1 "withoutUART/G__this_mem_m_1.log"
+`define MEM_INIT_G__this_mem_m_2 "withoutUART/G__this_mem_m_2.log"
+`define MEM_INIT_G__this_mem_m_3 "withoutUART/G__this_mem_m_3.log"
