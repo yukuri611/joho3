6e0d3f1fd6240391c248f8b614b062ff8fdec363
yukuri611 <yukiiw611@gmail.com>
2025-05-15 12:08:56 +0900

    コネクション付きのマンカラが作成完了

--- ex3_rv32/rv_test/C2R_ROOT/RTL_V_TB/RV32I_ARCH1_step_MemInitFileName.v
+++ ex3_rv32/rv_test/C2R_ROOT/RTL_V_TB/RV32I_ARCH1_step_MemInitFileName.v
@@ -1,4 +1,4 @@
-`define MEM_INIT_G__this_mem_m_0 "withoutUART/G__this_mem_m_0.log"
-`define MEM_INIT_G__this_mem_m_1 "withoutUART/G__this_mem_m_1.log"
-`define MEM_INIT_G__this_mem_m_2 "withoutUART/G__this_mem_m_2.log"
-`define MEM_INIT_G__this_mem_m_3 "withoutUART/G__this_mem_m_3.log"
+`define MEM_INIT_G__this_mem_m_0 "mancala_with_connection/G__this_mem_m_0.log"
+`define MEM_INIT_G__this_mem_m_1 "mancala_with_connection/G__this_mem_m_1.log"
+`define MEM_INIT_G__this_mem_m_2 "mancala_with_connection/G__this_mem_m_2.log"
+`define MEM_INIT_G__this_mem_m_3 "mancala_with_connection/G__this_mem_m_3.log"
